22a5c10893f44186db92ae3fc7eb04fa574fd93f
Zach <zjsievers@gmail.com>
2020-08-19 11:49:24 -0400

    changed state to app.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-      {
-        id: '2',
-        login: 'Zach',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-      {
-        id: '3',
-        login: 'jim',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
